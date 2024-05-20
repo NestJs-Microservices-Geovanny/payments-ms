aabbc9057c96f18569cbca5232596d918cc3321c
Geovanny Bucay <ebucayle@pichincha.com>
2024-05-19 20:08:26 -0500

    Stripe-webhook-hookdeck-forwarder

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 
 async function bootstrap() {
   const logger = new Logger('Payments-ms');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(envs.port);
 
   logger.log(`Microservices Payments running on port ${envs.port}`);
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,34 +1,72 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
 import Stripe from 'stripe';
 import { envs } from 'src/config';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
-  async createPaymentSession() {
-    const session = await this.stripe.checkout.sessions.create({
-      //aqui se coloca el iD de mi orden generada
-      payment_intent_data: {},
-      line_items: [
-        {
-          price_data: {
-            currency: 'USD',
-            product_data: {
-              name: 'T-shirt',
-            },
-            unit_amount: 2000, //20 dolares 2000/ 100 = 20.00
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items, orderId } = paymentSessionDto;
+    const lineItems = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: item.name,
           },
-          quantity: 2,
+          unit_amount: Math.round(item.price * 100),
         },
-      ],
+        quantity: item.quantity,
+      };
+    });
+    const session = await this.stripe.checkout.sessions.create({
+      payment_intent_data: {
+        metadata: {
+          orderId,
+        },
+      },
+      line_items: lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:3003/payments/success',
-      cancel_url: 'http://localhost:3003/payments/cancel',
+      success_url: envs.stripeSuccessUrl,
+      cancel_url: envs.stripeCancelUrl,
     });
 
     return session;
+  }
+
+  async stripeWebHook(req: Request, resp: Response) {
+    const sig = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    const endpointSecret = envs.stripeEndpointSecret;
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
+    } catch (err) {
+      resp.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceeded = event.data.object;
+        console.log({
+          metadata: chargeSucceeded.metadata,
+        });
+        break;
+      default:
+        console.log(`Event ${event.type} not handled`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    return resp.status(200).json({ sig });
   }
 }
